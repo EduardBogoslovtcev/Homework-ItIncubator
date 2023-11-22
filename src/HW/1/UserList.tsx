fd7a1113b870c39ab289d3025c7c0f0f16c82ffe
EduardBogoslovtcev <edik.bogoslovtcev@gmail.com>
2023-11-21 18:00:31 -0600

    test 02

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -17,7 +17,7 @@ type UserListPropsType = {
 export const UserList = (props: UserListPropsType) => {
   return (
     <div id={'hw01-users'}>
-      <h2>User List:</h2>
+      <h2>User List1:</h2>
 
       <ul>
        {props.users.map((user: UserType) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
